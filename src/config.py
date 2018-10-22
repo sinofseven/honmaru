0b28ff16776526a7c5baac96e0c919f7c213457e
sinofseven <em.s.00001@gmail.com>
2018-10-22 18:42:16 +0900

    implement read_honmaru_yml

--- src/config.py
+++ src/config.py
@@ -1,4 +1,12 @@
+import yaml
+
+
+def read_honmaru_yml():
+    with open('honmaru.yml') as f:
+        data = yaml.load(f)
+    return dict(data) if data is not None else {}
+
+
 class Config(object):
     def __init__(self):
-        with open('honmaru.yml'):
-            pass
+        read_honmaru_yml()
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,6 +1,45 @@
 import pytest
 
-from config import Config
+from config import Config, read_honmaru_yml
+
+
+class TestReadHonmaruYml(object):
+    def test_not_exist_honmaru_yml(self):
+        with pytest.raises(FileNotFoundError):
+            read_honmaru_yml()
+
+    @pytest.mark.parametrize(
+        'put_honmaru_yml, expected', [
+            (
+                'empty.yml',
+                {}
+            ),
+            (
+                'read_1.yml',
+                {
+                    'int': 1,
+                    'bool': True,
+                    'str': 'tttt',
+                    'array': [1, 2, 3],
+                    'dict': {
+                        'int': 1,
+                        'bool': True,
+                        'str': 'tttt'
+                    }
+                }
+            ),
+            (
+                'read_2.yml',
+                {
+                    'profile': 'test-profile',
+                    'region': 'ap-northeast-1'
+                }
+            )
+        ], indirect=['put_honmaru_yml']
+    )
+    def test_exist_file(self, put_honmaru_yml, expected):
+        actual = read_honmaru_yml()
+        assert actual == expected
 
 
 class TestConfig(object):
@@ -10,8 +49,8 @@ class TestConfig(object):
 
     @pytest.mark.parametrize(
         'put_honmaru_yml', [
-            ('init.yml')
-        ], indirect=True
+            ('empty.yml')
+        ], indirect=['put_honmaru_yml']
     )
     def test_exist_file(self, put_honmaru_yml):
         Config()
