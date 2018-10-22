dfb5e89c898fa142108720276de465fd3ef57ad6
sinofseven <em.s.00001@gmail.com>
2018-10-22 15:58:13 +0900

    change use to yield

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,10 +5,9 @@ import pytest
 
 @pytest.fixture(scope='function')
 def put_honmaru_yml(request):
-    def delete_file():
-        subprocess.call(['rm', 'honmaru.yml'])
-
     filename = 'fixture/honmaru.yml/{0}'.format(request.param)
     subprocess.call(['cp', filename, 'honmaru.yml'])
 
-    request.addfinalizer(delete_file)
+    yield
+
+    subprocess.call(['rm', 'honmaru.yml'])
